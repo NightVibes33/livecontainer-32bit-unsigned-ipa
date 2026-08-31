10810b0c09427ab6c9f824783531df9a76abc1e9
Zyn <bobbytatum999@gmail.com>
2026-08-31 16:32:55 -0500

    Add weak legacy NSLocale key exports

--- .github/scripts/patch-liveexec32-corefoundation.py
+++ .github/scripts/patch-liveexec32-corefoundation.py
@@ -5,6 +5,27 @@ source = path.read_text()
 anchor = 'NSString * const NSGenericException = @"NSGenericException";'
 if source.count(anchor) != 1:
     raise SystemExit("expected exactly one CoreFoundation Foundation-spellings anchor")
+locale_constants = """__attribute__((weak)) NSString * const NSLocaleIdentifier = @"identifier";
+__attribute__((weak)) NSString * const NSLocaleLanguageCode = @"language code";
+__attribute__((weak)) NSString * const NSLocaleCountryCode = @"country code";
+__attribute__((weak)) NSString * const NSLocaleScriptCode = @"script code";
+__attribute__((weak)) NSString * const NSLocaleVariantCode = @"variant code";
+__attribute__((weak)) NSString * const NSLocaleExemplarCharacterSet = @"exemplar character set";
+__attribute__((weak)) NSString * const NSLocaleCalendar = @"calendar";
+__attribute__((weak)) NSString * const NSLocaleCollationIdentifier = @"collation identifier";
+__attribute__((weak)) NSString * const NSLocaleUsesMetricSystem = @"uses metric system";
+__attribute__((weak)) NSString * const NSLocaleMeasurementSystem = @"measurement system";
+__attribute__((weak)) NSString * const NSLocaleDecimalSeparator = @"decimal separator";
+__attribute__((weak)) NSString * const NSLocaleGroupingSeparator = @"grouping separator";
+__attribute__((weak)) NSString * const NSLocaleCurrencySymbol = @"currency symbol";
+__attribute__((weak)) NSString * const NSLocaleCurrencyCode = @"currency code";
+__attribute__((weak)) NSString * const NSLocaleCollatorIdentifier = @"collator identifier";
+__attribute__((weak)) NSString * const NSLocaleQuotationBeginDelimiterKey = @"quotation begin delimiter";
+__attribute__((weak)) NSString * const NSLocaleQuotationEndDelimiterKey = @"quotation end delimiter";
+__attribute__((weak)) NSString * const NSLocaleAlternateQuotationBeginDelimiterKey = @"alternate quotation begin delimiter";
+__attribute__((weak)) NSString * const NSLocaleAlternateQuotationEndDelimiterKey = @"alternate quotation end delimiter";
+
+"""
 calendar_constants = """NSString * const NSBuddhistCalendar = @"buddhist";
 NSString * const NSChineseCalendar = @"chinese";
 NSString * const NSHebrewCalendar = @"hebrew";
@@ -17,4 +38,4 @@ NSString * const NSIndianCalendar = @"indian";
 NSString * const NSISO8601Calendar = @"iso8601";
 
 """
-path.write_text(source.replace(anchor, calendar_constants + anchor))
+path.write_text(source.replace(anchor, locale_constants + calendar_constants + anchor))
